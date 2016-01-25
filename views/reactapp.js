e427eca4dda9d1028b52365b34a5d89768506274
RICK STEELE <patrickjsteele@gmail.com>
2016-01-24 21:10:58 -0800

    front end gets data from API

--- views/reactapp.js
+++ views/reactapp.js
@@ -6,13 +6,38 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
 import {Motion, spring} from 'react-motion';
 
 const ReactApp = React.createClass ({
   getInitialState() {
     return {
       // todo: make initial state
+      notes: {},
+      txns: {},
     };
+  },
+
+  componentDidMount(){
+    const notesApi = '/api/notes';
+    $.get(notesApi, function(data){
+      if(this.isMounted()){
+        this.setState({
+          notes: data,
+        });
+        console.log(this.state.notes);
+      }
+    }.bind(this));
+
+    const txnsApi = '/api/transactions';
+    $.get(txnsApi, function(data){
+      if(this.isMounted()){
+        this.setState({
+          txns: data,
+        });
+        console.log(this.state.txns);
+      }
+    }.bind(this));
   },
 
   render: function(){
