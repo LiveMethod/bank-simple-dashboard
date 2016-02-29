b6c51925152e1d25a64a59764eea6c00d1b4ac01
RICK STEELE <patrickjsteele@gmail.com>
2016-02-28 17:26:41 -0800

    /scrape now scrapes

--- expressApp.js
+++ expressApp.js
@@ -11,16 +11,11 @@ var request = require('request');
 var index = require('./routes/index');
 var transactions = require('./routes/transactions');
 var notes = require('./routes/notes');
+var scraper = require('./routes/scraper');
 
 var expressLess = require('express-less');
 
 var expressApp = express();
-
-// these are just for the sketchy txn adding
-var mongoose = require('mongoose');
-var Transaction = require('./models/Transaction.js');
-
-var secrets = require('./secrets/secrets');
 
 // connect to mongodb
 mongoose.connect('mongodb://localhost/simple-bank-backend', function(err) {
@@ -46,6 +41,7 @@ expressApp.use(express.static(path.join(__dirname, 'public')));
 
 // link  URLs to their routes
 expressApp.use('/', index);
+expressApp.use('/scrape', scraper);
 expressApp.use('/api/transactions', transactions);
 expressApp.use('/api/notes', notes);
 
@@ -54,116 +50,6 @@ expressApp.use('/api/notes', notes);
 ///////
 ///////
 ///////
-
-var apiBase = 'https://bank.simple.com';
-var localhost = 'http://localhost:3000'
-var csrfRegex = /<meta name="_csrf" content="(.*)">/;
-
-// login credentials for simple.com (stored in secrets dir)
-user = secrets.simpleUser;
-pass = secrets.simplePass;
-// cookie jar! Stores csrf and other session stuff
-jar = request.jar();
-
-// get the CSRF token (accepts callback)
-function getCSRF(cb){
-  console.log('starting csrf');
-  // new request, specifying URL and cookie jar
-  request({url: apiBase, jar: jar}, function(err,res,body){
-    // if there's an error, yell
-    if (err) {console.log('get csrf err: ', err);}
-    // otherwise, perform the callback, passing null to the
-    // first spot (traditionally used for errors. if we got here, there are none)
-    // and passing the formatted CSRF token as the second param.
-    console.log('done csrf?');
-    if(cb != undefined) cb(null, csrfRegex.exec(body)[1]);
-  })
-}
-
-function login(cb){
-  console.log('attepting login.');
-
-  var requestOptions = {
-    method: 'POST',
-    url: apiBase + '/signin',
-    jar: jar,
-    form: {
-      username: user,
-      password: pass,
-    },
-  };
-
-  // call the CSRF method, passing in the login guts as
-  // a callback, so that they execute only once the token
-  // request comes back successfully.
-  getCSRF(function(err,token){
-    // add the csrf token to the form object
-    requestOptions.form._csrf = token;
-
-    console.log('starting login');
-    // new request with the options
-    request(requestOptions, function(err,res,body){
-      if (err) {console.log('login err: ', err);}
-      if (res.statusCode !== 303){console.log('Login rejected: '+res.statusCode);}
-      else {console.log('you sholud be logged in.')}
-      // do whatever next thing was passed in. 
-      if(cb != undefined)cb();
-    })
-  })
-}
-
-function getTransactions(){
-  var requestOptions = {
-    url: apiBase + '/transactions/data?all=true',
-    jar: jar,
-    json: true,
-  };
-
-  console.log('starting transactions get');
-  request(requestOptions, function(err,res,body){
-    if (err) {console.log('err getting transactions: ', err);}
-    if (res.statusCode === 302) {console.log('U ant loged n breh', err);}
-    if (res.statusCode !== 200) {console.log('status code ', res.statusCode);}
-    if (res.statusCode === 200) {
-      console.log('Got ' + body.transactions.length + ' transactions')
-      for (var transaction in body.transactions){
-        addTransactionWithoutAPI(body.transactions[transaction]);
-      }
-    }
-  });
-}
-
-function addTransactionWithoutAPI(transaction){
-  var query = {uuid: transaction.uuid}
-  var options = {
-    upsert:true, // create if it doesn't exist
-    new:true, // return new changed object, not old one
-  };
-
-  Transaction.findOneAndUpdate(query, transaction, options, function(err, transaction){
-    if (transaction.uuid == null){
-      console.log('null transaction ID. This should never happen: ', transaction )
-      return null;
-    }
-    if(err){
-      if (err.code === 11000){
-        console.log('Transaction already exists with key ');
-        return null;
-      } else {
-        console.log('error when creating this transaction: ', err)
-        return null;
-      }
-    }
-    console.log('upserting transaction ', transaction.uuid);
-  });
-}
-
-// Scrape all simple txns into the db
-// login(getTransactions);
-
-function confirmAwesomeness(){
-  console.log('confirmed: you are awesome');
-}
 
 // catch 404 and forward to error handler
 expressApp.use(function(req, res, next) {
